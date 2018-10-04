c21a8077c80754c9fee7f6ad5ebaca2fd3447366
mecaota <b1014167@fun.ac.jp>
2018-10-04 16:01:30 +0900

    reggps.sol's all function is completed to create.

--- reggps.sol
+++ reggps.sol
@@ -1,9 +1,10 @@
 pragma solidity ^0.4.25;
 contract GpsSpotRegister {
 	struct Spot{
+		uint256 index;
+		string name;
 		int32 x;
 		int32 y;
-		string name;
 	}
 	mapping (bytes32 => Spot) public spot_list;
 	bytes32[] public spotname_list;
@@ -16,10 +17,11 @@ contract GpsSpotRegister {
 			result := mload(add(source, 32))
 		}
 	}
-	function set(int32 _x, int32 _y, string __spotname) public{
+	function set(string __spotname, int32 _x, int32 _y) public returns (uint256){
 		bytes32 bytes_str = stringToBytes32(__spotname);
-		spot_list[bytes_str] = Spot(_x, _y, __spotname);
-		spotname_list.push(bytes_str);
+		uint256 raw_index = spotname_list.push(bytes_str)-1;
+		spot_list[bytes_str] = Spot(raw_index, __spotname, _x, _y);
+		return raw_index;
 	}
 	function get_x(string __spotname) public view returns (int32){
 		return spot_list[stringToBytes32(__spotname)].x;
@@ -27,9 +29,9 @@ contract GpsSpotRegister {
 	function get_y(string __spotname) public view returns (int32){
 		return spot_list[stringToBytes32(__spotname)].y;
 	}
-	function get_spot(string __spotname) public view returns (string, int32, int32){
-		bytes32 bytes_str = stringToBytes32(__spotname);
-		return (spot_list[bytes_str].name, spot_list[bytes_str].x, spot_list[bytes_str].y);
+	function get_spot(string __spotname) public view returns (uint256, string, int32, int32){
+		Spot target = spot_list[stringToBytes32(__spotname)];
+		return (target.index, target.name, target.x, target.y);
 	}
 	function get_spotlist() public view returns (bytes32[]){
 		return spotname_list;
