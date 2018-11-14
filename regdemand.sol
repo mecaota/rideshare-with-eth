5f37d7bab8568e2169634fc3fc5d2303c8af3f3a
mecaota <b1014167@fun.ac.jp>
2018-11-14 21:03:12 +0900

    change struct type, but not complete

--- regdemand.sol
+++ regdemand.sol
@@ -1,37 +1,92 @@
-pragma solidity ^0.4.25;
+pragma solidity ^0.4.26;
 pragma experimental ABIEncoderV2;
 contract Demand_Controller {
+    
     struct Spot{
         string name;
         int32 x;
         int32 y;
     }
+    
     struct Demand{
-        uint64 reg_date;
-        uint64 est_date;
+        bool enabled;
+        uint reg_date;
+        uint est_date;
         Spot dept;
         Spot arrv;
-        uint8 passengers;
+        uint passengers;
+        address[] members;
         address owner;
     }
-    Demand[] demand_list ;
-    function set(uint8 __passengers, uint64 __reg_date, uint64 __est_date, string dept_name, int32 dept_x, int32 dept_y, string arrv_name, int32 arrv_x, int32 arrv_y) public returns (uint){
-        return demand_list.push(Demand(__reg_date, __est_date, Spot(dept_name, dept_x, dept_y), Spot(arrv_name, arrv_x, arrv_y), __passengers, msg.sender))-1;
+    
+    mapping(address => uint) demand_index;
+    Demand[] demand_list;
+    
+    // コンストラクタ
+    constructor () public {
+        demand_list.push(
+            Demand({
+                enabled: false,
+                reg_date: 0,
+                est_date: 0,
+                dept: Spot({name: "", x: 0, y: 0}),
+                arrv: Spot({name: "", x: 0, y: 0}),
+                passengers: 0,
+                members: address[],
+                owner: msg.sender
+            })
+            );
     }
-    function get_deptspot(uint __index) public view returns (string, int32, int32){
-        Spot memory spot = demand_list[__index].dept;
-        return (spot.name, spot.x, spot.y);
+    
+    function set(
+    uint __passengers,
+    uint64 __est_date,
+    string dept_name,
+    int32 dept_x,
+    int32 dept_y,
+    string arrv_name,
+    int32 arrv_x,
+    int32 arrv_y)
+    public returns (bool){
+        require(demand_index[msg.sender]==0);
+        demand_index[msg.sender] = demand_list.push(
+            Demand({
+                enabled: true,
+                reg_date: now,
+                est_date: __est_date,
+                dept: Spot({name: dept_name, x: dept_x, y: dept_y}),
+                arrv: Spot({name: arrv_name, x: arrv_x, y: arrv_y}),
+                passengers: __passengers,
+                members: address[],
+                owner: msg.sender
+                
+            })
+            );
+        return true;
     }
-    function get_arrvspot(uint __index) public view returns (string, int32, int32){
-        Spot memory spot = demand_list[__index].arrv;
-        return (spot.name, spot.x, spot.y);
+    
+    function delete_demand(address owner) public returns (bool) {
+        uint __index = demand_index[msg.sender];
+        require(__index!=0);
+        delete demand_index[msg.sender];
+        demand_list[__index].enabled = false;
+        return true;
     }
-    function get_demand(uint __index) public view returns (uint64, uint64, uint8, string, int32, int32, string, int32, int32){
+    
+    function change_passengers(uint __index) public returns (bool) {
+        Demand memory demand = demand_list[__index];
+        require(demand.enabled==true && demand.passengers > 0);
+        demand_list[__index].passengers--;
+        return true;
+    }
+    
+    function get_demand(uint __index) public view returns (uint, uint, uint8, string, int32, int32, string, int32, int32){
         Demand memory demand = demand_list[__index];
         Spot memory dept_spot = demand.dept;
         Spot memory arrv_spot = demand.arrv;
         return (demand.reg_date, demand.est_date, demand.passengers, dept_spot.name, dept_spot.x, dept_spot.y, arrv_spot.name, arrv_spot.x, arrv_spot.y);
     }
+    
     function get_demandlist() public view returns (Demand[]){
         return demand_list;
     }
